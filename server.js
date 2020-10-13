8b05a50ce68c4b296197c50f2f77335ddf878eda
JoseOgam <jotienoochieng@gmail.com>
2020-10-13 13:28:55 -0400

    backend api enpoint

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors")
 require("./server/db/mongoose");
 var registerRouter = require("./server/router/registerRouter");
 
@@ -7,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors())
 app.use(registerRouter);
 app.listen(port, () => {
   console.log(`server is running at port ${port}`);
