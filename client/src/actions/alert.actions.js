f9a23013cb980c6cf628906a2f08928a3173a1eb
JoseOgam <jotienoochieng@gmail.com>
2020-10-28 09:21:22 -0400

    alert actions initial commit

---
+++ client/src/actions/alert.actions.js
@@ -0,0 +1,25 @@
+import { alertConstants } from "../constants"
+
+export const alertActions = {
+    success,
+    error,
+    clear
+}
+
+const success = (message) => {
+    return {
+        type: alertConstants.SUCCESS, message
+    }
+}
+
+const error = (message) => {
+    return {
+        type: alertConstants.ERROR, message
+    }
+}
+
+const clear = (message) => {
+    return {
+        type: alertConstants.CLEAR, message
+    }
+}
